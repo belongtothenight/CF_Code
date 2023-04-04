ef79650b4899af74081798481d5216798b0cc77a
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-04-04 08:31:39 +0800

    Update line.cs

--- src/LSystem/c#/line.cs
+++ src/LSystem/c#/line.cs
@@ -9,18 +9,38 @@ public class line : MonoBehaviour
     private LineRenderer lineRenderer;
     public List<Vector2> positions = new List<Vector2>(); // array of positions to follow
     private int currentPositionIndex = 0; // index of the current position in the array
-    public string path = @"D:\Note_Database\Subject\CF Chaos and Fractals\hw3\Q0_l5.csv"; // path to the CSV file
+    public string path = @"D:\Note_Database\Subject\CF Chaos and Fractals\hw3\Q3_l5.csv"; // path to the CSV file
     public static bool lineFullyDrawn = false;
     public static int fps = 60; // frames per second
     public float updateFrequency = 1 / fps; // how often to update the line
     private float timer;
+    public string task = "UpdateLine"; // "UpdateLine" or "DrawAllLines"
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Start");
-        Debug.Log(path);
-        // Load CSV file
+        LoadCoordinate();
+        OffsetAllCoordinate();
+        SetLineRendererSettings();
+        Debug.Log("Start Done");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (task == "UpdateLine")
+            UpdateLine();
+        else if (task == "DrawAllLines")
+            DrawAllLines();
+
+        // Debug.Log($"Frame: {Time.frameCount}");
+    }
+
+    void LoadCoordinate()
+    {
+        // load the CSV file and add each position to the positions array
+        Debug.Log($"Loading {path}...");
         using (var reader = new StreamReader(path))
         {
             int i = 0;
@@ -40,12 +60,16 @@ public class line : MonoBehaviour
                 }
             }
         }
-        // Debug.Log(positions.Count);
         Debug.Log($"Loaded {positions.Count} positions from CSV file.");
+    }
 
-        // get top-most and right-most position
+    void OffsetAllCoordinate()
+    {
+        // get top-most, right-most, left-most, and bottom-most positions
         Vector2 topMost = positions[0];
         Vector2 rightMost = positions[0];
+        Vector2 leftMost = positions[0];
+        Vector2 bottomMost = positions[0];
         foreach (Vector2 position in positions)
         {
             if (position.y > topMost.y)
@@ -57,42 +81,47 @@ public class line : MonoBehaviour
             {
                 rightMost = position;
             }
+
+            if (position.x < leftMost.x)
+            {
+                leftMost = position;
+            }
+
+            if (position.y < bottomMost.y)
+            {
+                bottomMost = position;
+            }
         }
 
-        // offset all positions by the top-most and right-most position
-        Vector2 offset = new Vector2(-rightMost.x / 2, -topMost.y / 2);
+        Debug.Log($"Top: {topMost}");
+        Debug.Log($"Right: {rightMost}");
+        Debug.Log($"Left: {leftMost}");
+        Debug.Log($"Bottom: {bottomMost}");
+
+        // offset all positions by the midpoint of the top, right, left, and bottom-most positions
+        Vector2 offset = new Vector2((rightMost.x + leftMost.x) / 2, (topMost.y + bottomMost.y) / 2);
         for (int i = 0; i < positions.Count; i++)
         {
-            positions[i] += offset;
+            positions[i] -= offset;
         }
+    }
 
+    void SetLineRendererSettings()
+    {
         // get the LineRenderer component and set its settings
         lineRenderer = gameObject.AddComponent<LineRenderer>();
         lineRenderer.startWidth = 0.02f;
         lineRenderer.endWidth = 0.02f;
         lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
         lineRenderer.positionCount = positions.Count;
-        // Application.targetFrameRate = fps;
-        Debug.Log("Start Done");
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // UpdateLine();
-        DrawAllLines();
-
-        // Debug.Log($"Frame: {Time.frameCount}");
-    }
-
 
     void UpdateLine()
     {
-        // animate line drawing
-        // update the line every updateFrequency seconds to prevent weird artifacts
         timer -= Time.deltaTime;
         if (timer <= 0)
         {
+            // update the line every updateFrequency seconds to prevent weird artifacts
             if (currentPositionIndex < positions.Count)
             {
                 lineRenderer.SetPosition(currentPositionIndex, positions[currentPositionIndex]);
@@ -108,7 +137,6 @@ public class line : MonoBehaviour
 
     void DrawAllLines()
     {
-        // draw the entire line at once
         for (int i = 0; i < positions.Count; i++)
         {
             lineRenderer.SetPosition(i, positions[i]);
