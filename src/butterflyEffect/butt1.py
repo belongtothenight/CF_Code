1414012eb9f49100f3eac97fde2f1d55aed931ac
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-06-23 15:15:42 +0800

    Remove unneeded parameters

--- src/butterflyEffect/butt1.py
+++ src/butterflyEffect/butt1.py
@@ -2,14 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-x0 = 0
 minN = 0
 maxN = 250
 c = -2
-x0 = 0.1
-s = 1
-hist = [0.1]
-hist1 = [0.11]
+x0_0 = 0.1
+x0_1 = 0.11
+hist = [x0_0]
+hist1 = [x0_1]
 
 for n in range(minN, maxN):
     y = hist[-1] ** 2 + c
@@ -27,7 +26,6 @@ hist1 = hist1[0:-1]
 print()
 plt.xlabel("n")
 plt.ylabel("Xn")
-# plt.scatter(range(minN, maxN), hist, s=s, color="blue")
 plt.plot(range(minN, maxN), hist)
 plt.plot(range(minN, maxN), hist1)
 plt.show()
